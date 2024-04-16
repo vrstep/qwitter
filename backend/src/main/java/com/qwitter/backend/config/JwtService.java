38b1ef88e08f73bafd0c78655c5d01cf00c0e338
Amir <amir.batyrggaliev@gmail.com>
2024-04-16 18:27:34 +0500

    Setup extractClaims

--- backend/src/main/java/com/qwitter/backend/config/JwtService.java
+++ backend/src/main/java/com/qwitter/backend/config/JwtService.java
@@ -1,10 +1,39 @@
 package com.qwitter.backend.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "bb6c91cbce311a469318a124111429aaefe1e860eb7e2d6bb7afba56686e832e";
+
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
