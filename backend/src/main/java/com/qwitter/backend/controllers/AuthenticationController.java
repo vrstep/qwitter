d61f57bbec5380f1c6374df887fe71b703c55553
Amir <amir.batyrggaliev@gmail.com>
2024-05-10 20:47:10 +0500

    Add authentication check endpoint

--- backend/src/main/java/com/qwitter/backend/controllers/AuthenticationController.java
+++ backend/src/main/java/com/qwitter/backend/controllers/AuthenticationController.java
@@ -6,18 +6,15 @@ import com.qwitter.backend.dto.RegisterRequest;
 import com.qwitter.backend.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-
     private final AuthenticationService service;
-
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
@@ -27,5 +24,11 @@ public class AuthenticationController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<Boolean> checkAuth() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return ResponseEntity.ok(auth != null && auth.isAuthenticated());
     }
 }
