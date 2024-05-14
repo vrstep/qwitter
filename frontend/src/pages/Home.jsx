f6b62295d8081fe2f08ab9b1c01f79600e7a4c05
Amir <amir.batyrggaliev@gmail.com>
2024-05-14 19:05:18 +0500

    Create main component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import * as React from "react";
 import LeftSidebar from "../components/LeftSidebar";
-import { Link } from "react-router-dom";
-
-
+import MainComponent from "../components/MainComponent";
 
 function Home() {
   return (
     <div className="w-full h-full flex justify-center items-center relative">
       <div className="max-w-screen-xl w-full h-full flex relative">
-        <LeftSidebar />        
-        <main></main>
+        <LeftSidebar />
+        <MainComponent />
         <section></section>
       </div>
     </div>
