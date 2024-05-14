98be638881b58fa0b0eaf87887876133fa913e2f
Amir <amir.batyrggaliev@gmail.com>
2024-05-14 19:14:35 +0500

    Add right sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import LeftSidebar from "../components/LeftSidebar";
 import MainComponent from "../components/MainComponent";
+import Rightsidebar from "../components/Rightsidebar";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <div className="max-w-screen-xl w-full h-full flex relative">
         <LeftSidebar />
         <MainComponent />
+        <Rightsidebar />
         <section></section>
       </div>
     </div>
