47ab93cbe5b41bde89fd84c64e185fc96e68fc80
Amir <amir.batyrggaliev@gmail.com>
2024-05-27 18:13:23 +0500

    Add crud endpoints for users

--- backend/src/main/java/com/qwitter/backend/controllers/UserController.java
+++ backend/src/main/java/com/qwitter/backend/controllers/UserController.java
@@ -1,10 +1,13 @@
 package com.qwitter.backend.controllers;
 
+import com.qwitter.backend.dto.ChangePasswordRequest;
 import com.qwitter.backend.models.User;
 import com.qwitter.backend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -32,5 +35,26 @@ public class UserController {
     public void deleteUserById(@PathVariable Integer id) {
         log.info("Deleting a user by id: {}", id);
         userService.deleteUserById(id);
+    }
+
+    @PutMapping("/changeEmail/{id}")
+    public User changeEmail(@PathVariable Integer id, @RequestBody String email) {
+        log.info("Changing email for user with id: {}", id);
+        return userService.changeEmail(id, email);
+    }
+
+    @PutMapping("/changeUsername/{id}")
+    public User changeUsername(@PathVariable Integer id, @RequestBody String username) {
+        log.info("Changing username for user with id: {}", id);
+        return userService.changeUsername(id, username);
+    }
+
+    @PutMapping("/changePassword/{id}")
+    public ResponseEntity<?> changePassword(
+            @RequestBody ChangePasswordRequest request,
+            Principal connectedUser
+    ) {
+        userService.changePassword(request, connectedUser);
+        return ResponseEntity.ok().build();
     }
 }
