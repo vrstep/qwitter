3896fff46ad728da832fe71baf3227f429ce80c2
Amir <amir.batyrggaliev@gmail.com>
2024-05-27 18:15:07 +0500

    Add find by id in userrepo

--- backend/src/main/java/com/qwitter/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/qwitter/backend/repositories/UserRepository.java
@@ -8,4 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
+    Optional<User> findByUserName(String userName);
+
+    Optional<User> findById(Integer userId);
 }
