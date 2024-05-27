d714fae5b8e98b26d7242535a5670229a41e4cbf
Amir <amir.batyrggaliev@gmail.com>
2024-05-27 18:12:01 +0500

    Add logout

--- backend/src/main/java/com/qwitter/backend/controllers/AuthenticationController.java
+++ backend/src/main/java/com/qwitter/backend/controllers/AuthenticationController.java
@@ -3,11 +3,15 @@ package com.qwitter.backend.controllers;
 import com.qwitter.backend.dto.AuthenticationRequest;
 import com.qwitter.backend.dto.AuthenticationResponse;
 import com.qwitter.backend.dto.RegisterRequest;
+import com.qwitter.backend.models.User;
 import com.qwitter.backend.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
@@ -30,5 +35,20 @@ public class AuthenticationController {
     public ResponseEntity<Boolean> checkAuth() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return ResponseEntity.ok(auth != null && auth.isAuthenticated());
+    }
+
+    @GetMapping("/currentuser")
+    public ResponseEntity<User> getCurrentUser() {
+        User currentUser = authenticationService.getCurrentUser();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return ResponseEntity.ok().build();
     }
 }
