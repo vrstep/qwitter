8057004ecc171bac3c6d60d5a12f8ac6c16017e7
Amir <amir.batyrggaliev@gmail.com>
2024-07-04 19:17:29 +0500

    Add packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { AuthContext } from './contexts/AuthContext';
 import { BrowserRouter as Router, Route, Navigate, BrowserRouter, Routes } from 'react-router-dom';
 import Welcome from './pages/Welcome';
 import Home from './pages/Home';
+import Settings from './pages/Settings';
+import ProfilePage from './pages/ProfilePage';
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -18,6 +21,8 @@ function App() {
       <Routes>
         <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Welcome />} />
         <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/" />} />
+        <Route path="/settings" Component={Settings} />
+        <Route path='/profile' Component={ProfilePage} />
       </Routes>
     </BrowserRouter>
     </AuthContext.Provider>
